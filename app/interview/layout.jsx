252da84e85346c07097669879da9b7fb835fc550
Himanshu Gupta <himanshu561hi@gmail.com>
2025-09-27 14:17:47 +0530

    Update layout.jsx

--- app/interview/layout.jsx
+++ app/interview/layout.jsx
@@ -1,32 +1,13 @@
-// "use client"
-// import React from 'react'
-// import InterviewHeader from './_components/InterviewHeader'
-// import { InterviewDataContext } from '@/context/InterviewDataContext'
-// import { useState } from 'react'
 
-// function InterviewLayout({ children }) {
-//   const [InterviewInfo, setInterviewInfo] = useState();
-//   return (
-//     <InterviewDataContext.Provider value={{InterviewInfo, setInterviewInfo}}>
-//     <div className="bg-gray-100">
-//         <InterviewHeader />
-//         {children}
-//     </div> 
-//     </InterviewDataContext.Provider>
-//   )
-// }
-
-// export default InterviewLayout
-
-
-// app/layout.js
 import { InterviewDataProvider } from "@/context/InterviewDataContext";
+import InterviewHeader from "../../app/interview/_components/InterviewHeader";
 
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
+              <InterviewHeader/>
                 <InterviewDataProvider>{children}</InterviewDataProvider>
             </body>
         </html>
